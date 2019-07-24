d318f24014f1d000ba29382cfcccb2dc2dbd7716
Justis Blasco <justis.blasco@gmail.com>
2019-07-24 15:55:16 -0400

    Commit compiled scripts

---
+++ dist/builtin/filters/string.d.ts
@@ -0,0 +1,19 @@
+declare const _default: {
+    'append': (v: string, arg: string) => string;
+    'prepend': (v: string, arg: string) => string;
+    'capitalize': (str: string) => string;
+    'lstrip': (v: string) => string;
+    'downcase': (v: string) => string;
+    'upcase': (str: string) => string;
+    'remove': (v: string, arg: string) => string;
+    'remove_first': (v: string, l: string) => string;
+    'replace': (v: string, pattern: string, replacement: string) => string;
+    'replace_first': (v: string, arg1: string, arg2: string) => string;
+    'rstrip': (str: string) => string;
+    'split': (v: string, arg: string) => string[];
+    'strip': (v: string) => string;
+    'strip_newlines': (v: string) => string;
+    'truncate': (v: string, l?: number, o?: string) => string;
+    'truncatewords': (v: string, l?: number, o?: string) => string;
+};
+export default _default;
